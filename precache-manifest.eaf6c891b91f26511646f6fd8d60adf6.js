e0a606108ded1ff67829c537165488f2e1da664d
isane-hash <isanejossy@gmail.com>
2020-02-03 12:20:59 +0200

    Updates

---
+++ precache-manifest.eaf6c891b91f26511646f6fd8d60adf6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c460f31218c409f5c490",
+    "url": "/poketimes/static/css/main.2586a38b.chunk.css"
+  },
+  {
+    "revision": "c460f31218c409f5c490",
+    "url": "/poketimes/static/js/main.c460f312.chunk.js"
+  },
+  {
+    "revision": "ebef2bb4ff3905719ab2",
+    "url": "/poketimes/static/js/1.ebef2bb4.chunk.js"
+  },
+  {
+    "revision": "7b792e93ca24609537ee",
+    "url": "/poketimes/static/js/runtime~main.7b792e93.js"
+  },
+  {
+    "revision": "34a6cfd3bbc6860618fe8ad8bde5b406",
+    "url": "/poketimes/index.html"
+  }
+];
